8087cdbec5635f1f140e8f99a3a8f7c7144136ce
htanou26 <htanou@proton.me>
2023-03-04 03:36:39 +0100

    3 commit

--- server.js
+++ server.js
@@ -21,16 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://stock-management-frontend-sand.vercel.app/"],
+    origin: ["http://localhost:3000", "https://stock-management-frontend-ebon.vercel.app"],
     credentials: true,
   })
 );
-app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-
-  next();
-});
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
